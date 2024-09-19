f821b81ee232c3fa6417cf4b0f863d26a52ef4dc
Corbin Robeck <corbin.robeck@amd.com>
2024-09-19 04:50:14 +0000

    begin neutron code

---
+++ third_party/neutron/csrc/Neutron.cpp
@@ -0,0 +1,28 @@
+//#include "Proton.h"
+
+#include <map>
+#include <stdexcept>
+
+#include "pybind11/pybind11.h"
+#include "pybind11/stl.h"
+#include "pybind11/stl_bind.h"
+
+using namespace neutron;
+
+void initNeutron(pybind11::module &&m) {
+  using ret = pybind11::return_value_policy;
+  using namespace pybind11::literals;
+
+  m.def("start", [](size_t sessionId) {
+//    SessionManager::instance().activateSession(sessionId);
+  });
+
+  m.def("finalize", [](size_t sessionId) {
+ //   SessionManager::instance().deactivateSession(sessionId);
+  });
+}
+
+PYBIND11_MODULE(libneutron, m) {
+  m.doc() = "Python bindings to the Neutron API";
+  initNeutron(std::move(m.def_submodule("neutron")));
+}
