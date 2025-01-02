32ea826c6a286ae1d684c24acf143abed11a8aa9
Corbin Robeck <corbin.robeck@amd.com>
2025-01-02 02:15:14 +0000

    move TritonOpBuilder into stand alone header

--- third_party/proton/dialect/triton_proton.cc
+++ third_party/proton/dialect/triton_proton.cc
@@ -1,6 +1,7 @@
 #include "Dialect/Proton/IR/Dialect.h"
 #include "mlir/Pass/PassManager.h"
 #include "passes.h"
+#include "ir.h"
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 #include <pybind11/stl_bind.h>
@@ -9,6 +10,10 @@ namespace py = pybind11;
 
 void init_triton_proton(py::module &&m) {
   auto passes = m.def_submodule("passes");
+
+  py::class_<TritonOpBuilder>(m, "builder", py::module_local(),
+                              py::dynamic_attr())
+      .def(py::init<MLIRContext *>());
 
   // load dialects
   m.def("load_dialects", [](mlir::MLIRContext &context) {
